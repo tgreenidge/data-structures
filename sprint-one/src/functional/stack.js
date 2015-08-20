c0b46386a1614d27a40cef3e66b29db4c845a346
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2015-08-20 11:49:21 -0400

    solved stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function(){
   var someInstance = {};
-
+  var count = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[count] = value;
+    count ++;
   };
 
   someInstance.pop = function(){
+    if(count){
+    count --;
+    }
+    var result = storage[count];
+    delete storage[count];
+    return result;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
