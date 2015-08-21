675f897f5da417aa51b7a9e7a70e2678d8dfe4ea
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2015-08-20 20:39:39 -0400

    solved sprint 1 functional shared stack, protypal stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //var currentCount = 0;
+  var obj = {
+    count : 0,
+    storage: {}
+    };
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+
+var stackMethods = { 
+  push : function(value){
+  this.storage[this.count] = value; 
+  this.count++;      
+}, 
+pop : function(){
+  if(this.count){
+    this.count--;
+  };
+  var result = this.storage[this.count];
+  delete this.storage[this.count];
+  return result;
+},
+size : function(){
+  return this.count;
+}
+};
+
 
 
