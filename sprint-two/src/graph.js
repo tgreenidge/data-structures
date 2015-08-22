0836f7388525c43981082da8b6d15849fd0fd4bf
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2015-08-22 17:00:47 -0400

    solved graph.js and set.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -4,41 +4,82 @@
 
 // Instantiate a new graph
 var Graph = function(){
+  this.storage = {};
+  //storage.connections = [];
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node){
+  this.storage[node] = {connections : [] };
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node){
+  for(var k in this.storage){
+    if(k === node){
+      return true;
+    }
+  }
+  return false;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node){
+   for(var k in this.storage){
+    if(k === node){
+      delete this.storage[k];
+    }
+  }
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var hasToNodes = false;
+    for(var i = 0; i < this.storage[fromNode].connections.length; i++){
+      if(this.storage[fromNode].connections[i] === toNode){
+        hasToNodes = true;
+      }
+    }      
+  return hasToNodes;
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if(this.contains(fromNode) && this.contains(toNode)){
+    this.storage[fromNode].connections.push(toNode);
+    this.storage[toNode].connections.push(fromNode);
+  }
+  console.log(this.storage);
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  if((this.contains(fromNode) && this.contains(toNode))){
+    for(var i = 0; i < this.storage[fromNode].connections.length; i++){
+      if(this.storage[fromNode].connections[i] === toNode){
+        delete this.storage[fromNode].connections[i]; 
+        for(var j = 0; j < this.storage[toNode].connections.length; j++){
+          if(this.storage[toNode].connections[j] === fromNode){
+            delete this.storage[toNode].connections[j];
+          }
+        }
+      } 
+    } 
+  }      
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb){
+  for(var k in this.storage){
+    cb(k);
+  }
 };
 
 /*
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
+
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(!this.contains(item)){
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item){
+//check to see if item is in storage
+for(var i = 0; i < this._storage.length; i++){
+  if(this._storage[i] === item){
+    return true; 
+  }  
+}
+ return false;
 };
 
 setPrototype.remove = function(item){
+//loop through storage 
+  for(var i = 0; i < this._storage.length; i++){
+    if(this._storage[i] === item){
+      delete this._storage[i]; 
+    }  
+  }
 };
 
 /*
